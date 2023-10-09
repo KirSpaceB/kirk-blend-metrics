48388afd42ca202294c4445f710b43478ef74267
KirSpaceB <productivekirk@gmail.com>
2023-10-09 07:39:12 -0700

    fix: uncessary lines

--- components/ui/account-settings-info-page/account-info-settings-layout.tsx
+++ components/ui/account-settings-info-page/account-info-settings-layout.tsx
@@ -3,6 +3,7 @@ import AccountInfoSettingsNav from "./account-info-settings-nav";
 import PersonalInfoPageInnerSideBar from "../personal-info-page-inner-sidebar";
 import AccountIfoPageOuterSidebar from "./account-info-page-outer-sidebar";
 import PersonalInfoInterface from "./personal-info-interface";
+import { Button } from "../button";
 export default function AccountInfoSettingsLayout() {
   return (
     <>
--- components/ui/account-settings-info-page/personal-info-interface.tsx
+++ components/ui/account-settings-info-page/personal-info-interface.tsx
@@ -1,6 +1,6 @@
 import { Avatar, AvatarImage, AvatarFallback } from "../avatar";
 import { Input } from "../input";
-import { InputGroup } from "../input-group";
+
 import {
   Select,
   SelectTrigger,
@@ -8,25 +8,39 @@ import {
   SelectContent,
   SelectGroup,
   SelectItem,
-  SelectLabel,
-  SelectSeparator,
-  Trigger,
 } from "../select";
-import { InputRightElement } from "../input-element";
-import { HelpCircle } from "@/components/icons";
+
 import { Label } from "../label";
-import {
-  DropdownMenu,
-  DropdownMenuTrigger,
-  DropdownMenuCheckItem,
-  DropdownMenuContent,
-} from "../dropdown-menu";
-import { Button } from "../button";
-import { ChevronDown } from "lucide-react";
+
 import React from "react";
 import { Badge } from "../badge";
 import { AvatarPencil } from "@/components/icons";
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOption,
+  ListboxOptions,
+} from "../listbox";
+
+const people = [
+  { id: 0, name: "Kirk" },
+  { id: 1, name: "Kirk1" },
+  { id: 2, name: "Kirk2" },
+  { id: 3, name: "Kirk3" },
+  { id: 4, name: "Kirk4" },
+  { id: 5, name: "Kirk5" },
+];
+const languages = [
+  { id: 0, langname: "Language1" },
+  { id: 1, langname: "Language2" },
+  { id: 2, langname: "Language3" },
+  { id: 3, langname: "Language4" },
+  { id: 4, langname: "Language5" },
+];
 export default function PersonalInfoInterface() {
+  const [selected, setSelected] = React.useState<string>();
+  const [langState, setLangState] = React.useState<string>();
+
   return (
     <div className="pl-[370px] pt-[110px]">
       <div className="h-[580px] w-[664px]">
@@ -42,12 +56,16 @@ export default function PersonalInfoInterface() {
 
           <div id="Avatar Parent Div" className="mb-[32px] mt-[24px]">
             <div className="inline-flex items-center gap-x-3">
-              <Avatar size="2xl">
-                <AvatarImage alt="Man" src="/man.jpg" />
-                <AvatarFallback>M</AvatarFallback>
+              {/* CHange css make it hoverable with the pencil */}
+              <Avatar size="2xl" className="h-[96px] w-[96px]">
+                <AvatarImage alt="Man" src="" />
+                <AvatarFallback>CT</AvatarFallback>
               </Avatar>
 
-              <AvatarPencil className="h-[30px] w-[30px] rounded-full border-[1px] border-solid border-[#98A2B3] p-[6px] sm:absolute sm:left-[435px] sm:top-[280px]" />
+              <AvatarPencil
+                className="h-[30px] w-[30px] cursor-pointer rounded-full border-[1px] border-solid border-[#98A2B3] p-[6px] sm:absolute sm:left-[460px] sm:top-[300px]"
+                onClick={() => alert("test")}
+              />
               <div className="flex flex-col pl-6">
                 <div className="flex flex-row items-center gap-6">
                   <span className="text-2xl font-medium capitalize text-[#475467]">
@@ -125,28 +143,22 @@ export default function PersonalInfoInterface() {
                 </div>
               </div>
             </div>
-
-            <div className=" space-y-1.5">
+            <div className="space-y-1.5">
               <Label htmlFor="email" size="sm">
                 What team do you work on?
               </Label>
-              <div className="">
-                <Select defaultValue="US">
-                  <SelectTrigger className=" w-[320px] rounded-md">
-                    <SelectValue placeholder="Select a country" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectGroup>
-                      <SelectItem value="US">US</SelectItem>
-                      <SelectItem value="BE">BE</SelectItem>
-                      <SelectItem value="RS">RS</SelectItem>
-                      <SelectItem value="TR">TR</SelectItem>
-                      <SelectItem value="LV">LV</SelectItem>
-                    </SelectGroup>
-                  </SelectContent>
-                </Select>
-              </div>
+              <Listbox value={selected} onChange={setSelected}>
+                <ListboxButton placeholder="Select Team" />
+                <ListboxOptions className="z-10">
+                  {people.map((person) => (
+                    <ListboxOption key={person.name} value={person.name}>
+                      {person.name}
+                    </ListboxOption>
+                  ))}
+                </ListboxOptions>
+              </Listbox>
             </div>
+
             <div className="space-y-1.5">
               <Label htmlFor="email" size="sm">
                 Job Title
@@ -155,24 +167,21 @@ export default function PersonalInfoInterface() {
             </div>
             <div className="space-y-1.5">
               <Label htmlFor="email" size="sm">
-                Language
+                Language?
               </Label>
-              <div className="">
-                <Select defaultValue="US">
-                  <SelectTrigger className=" w-[320px] rounded-md">
-                    <SelectValue placeholder="Select a country" />
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectGroup>
-                      <SelectItem value="US">US</SelectItem>
-                      <SelectItem value="BE">BE</SelectItem>
-                      <SelectItem value="RS">RS</SelectItem>
-                      <SelectItem value="TR">TR</SelectItem>
-                      <SelectItem value="LV">LV</SelectItem>
-                    </SelectGroup>
-                  </SelectContent>
-                </Select>
-              </div>
+              <Listbox value={langState} onChange={setLangState}>
+                <ListboxButton placeholder="English" Fix />
+                <ListboxOptions>
+                  {languages.map((language) => (
+                    <ListboxOption
+                      key={language.langname}
+                      value={language.langname}
+                    >
+                      {language.langname}
+                    </ListboxOption>
+                  ))}
+                </ListboxOptions>
+              </Listbox>
             </div>
           </div>
         </div>
